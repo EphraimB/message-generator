82ca2d674c68d9def58bc046dbfaee918cef5600
Ephraim Becker <emb16@outlook.com>
2022-07-28 14:13:04 -0400

    Wrote 2 more jokes.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const cornyJokes = joke => {
     return {
-        cornyJokes: ["Do you know where people pick their noses? The Poconos!", "Do you know where eczema comes from? Eggs!", "Do you know where people get their clothing patched from? Patchougue!"]
+        cornyJokes: ["Do you know where people pick their noses? The Poconos!", "Do you know where eczema comes from? Eggs!", "Do you know where people get their clothing patched from? Patchougue!", "Do you know who made Ceasar salad? Julius Caesar!", "Do you know who made up joking? King Joe!"]
     }
 }
