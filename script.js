9da107e094f67bbfa6b0578d39dbc5cc363091ab
Ephraim Becker <emb16@outlook.com>
2022-07-28 14:13:48 -0400

    Took away the parameter.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const cornyJokes = joke => {
+const cornyJokes = () => {
     return {
         cornyJokes: ["Do you know where people pick their noses? The Poconos!", "Do you know where eczema comes from? Eggs!", "Do you know where people get their clothing patched from? Patchougue!", "Do you know who made Ceasar salad? Julius Caesar!", "Do you know who made up joking? King Joe!"]
     }
