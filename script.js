0cc85fca38cfec0034ef44d2e813cb5747ae045e
Ephraim Becker <emb16@outlook.com>
2022-07-28 14:22:34 -0400

    Now outputs a random joke to the console.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const cornyJokes = () => {
     return {
-        cornyJokes: ["Do you know where people pick their noses? The Poconos!", "Do you know where eczema comes from? Eggs!", "Do you know where people get their clothing patched from? Patchougue!", "Do you know who made Ceasar salad? Julius Caesar!", "Do you know who made up joking? King Joe!"]
+        cornyJokes: ["Do you know where people pick their noses? The Poconos!", "Do you know where eczema comes from? Eggs!", "Do you know where people get their clothing patched from? Patchougue!", "Do you know who made Ceasar salad? Julius Caesar!", "Do you know who made up joking? King Joe!"],
+        getRandomJoke() {
+            const randomNumber = Math.floor(Math.random() * this.cornyJokes.length);
+
+            return this.cornyJokes[randomNumber];
+        }
     }
 }
+const cornyJoke = cornyJokes();
+console.log(cornyJoke.getRandomJoke());
