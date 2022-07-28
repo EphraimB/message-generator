511c70c7cb749e83b7dddedf35b2a2607d7328a8
Ephraim Becker <emb16@outlook.com>
2022-07-28 14:10:51 -0400

    Made a factory function with a few corny jokes.

---
+++ script.js
@@ -0,0 +1,5 @@
+const cornyJokes = joke => {
+    return {
+        cornyJokes: ["Do you know where people pick their noses? The Poconos!", "Do you know where eczema comes from? Eggs!", "Do you know where people get their clothing patched from? Patchougue!"]
+    }
+}
